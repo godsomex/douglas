96e1d973d6dffb0985534a73117e4f90d9165845
Godspower <goc.omenihu@gmail.com>
2019-12-16 19:12:31 +0100

    webpack config for prod and visualization

---
+++ build-utils/webpack.prod.js
@@ -0,0 +1,10 @@
+const Dotenv = require('dotenv-webpack');
+module.exports = {
+  mode: 'production',
+  devtool: 'eval-source-map',
+  plugins: [
+    new Dotenv({
+      path: './.env.production',
+    })
+  ],
+};
