983786f2e22b629b2ab6096c0d1f506e8b4ea8c9
Godspower <goc.omenihu@gmail.com>
2019-12-22 23:11:46 +0100

    Add styles to Collection Compnents

--- src/components/collections/Collection.tsx
+++ src/components/collections/Collection.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import CollectionItem from '../collectionItem/CollectionItem';
+import styled from '@emotion/styled';
 
 function Collection({ title, items }) {
     return (
-        <div>
-            <h1>{title}</h1>
-            {items
-                .filter((item, idx) => idx < 4)
-                .map(({ id, ...otherItems }) => (
-                    <CollectionItem key={id} {...otherItems} />
-                ))}
-        </div>
+        <CollectionStyle>
+            <TitleStyle>{title}</TitleStyle>
+            <PreviewStyle>
+                {items
+                    .filter((item, idx) => idx < 4)
+                    .map(({ id, ...otherItems }) => (
+                        <CollectionItem key={id} {...otherItems} />
+                    ))}
+            </PreviewStyle>
+        </CollectionStyle>
     );
 }
 
 export default Collection;
+
+//Styles
+const CollectionStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 30px;
+`;
+
+const TitleStyle = styled.h1`
+    font-size: 28px;
+    margin-bottom: 25px;
+`;
+
+const PreviewStyle = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
