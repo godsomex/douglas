a206c5fa1c456d8e08af353a9cbf3aafc7a2c4a0
Godspower Omenihu <ask4godsomex@gmail.com>
2019-12-22 23:25:51 +0100

    Shop  (#6)
    * update shop pages and collection components
    
    * Add styles to Collection Compnents

--- src/components/collectionItem/CollectionItem.tsx
+++ src/components/collectionItem/CollectionItem.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
 function CollectionItem({ id, name, price, imageUrl }) {
     return (
-        <div className="collection-item">
+        <ItemStyle>
             <div
                 className="image"
                 style={{
@@ -13,8 +14,41 @@ function CollectionItem({ id, name, price, imageUrl }) {
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
             </div>
-        </div>
+        </ItemStyle>
     );
 }
 
 export default CollectionItem;
+
+const ItemStyle = styled.div`
+    width: 22%;
+    display: flex;
+    flex-direction: column;
+    height: 350px;
+    align-items: center;
+
+    .image {
+        width: 100%;
+        height: 95%;
+        background-size: cover;
+        background-position: center;
+        margin-bottom: 5px;
+    }
+
+    .collection-footer {
+        width: 100%;
+        height: 5%;
+        display: flex;
+        justify-content: space-between;
+        font-size: 18px;
+
+        .name {
+            width: 90%;
+            margin-bottom: 15px;
+        }
+
+        .price {
+            width: 10%;
+        }
+    }
+`;
