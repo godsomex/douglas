1010750edb687f4f3c689306edcbda1430a4b712
Godspower <goc.omenihu@gmail.com>
2019-12-25 04:44:38 +0100

    more styles

--- src/components/custom-button/CustomButton.tsx
+++ src/components/custom-button/CustomButton.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
 function CustomButton({ children, ...otherProps }) {
-    return <button {...otherProps}>{children}</button>;
+    return <ButtonStyle {...otherProps}>{children}</ButtonStyle>;
 }
 
 export default CustomButton;
+
+const ButtonStyle = styled.button`
+    min-width: 165px;
+    width: auto;
+    height: 50px;
+    letter-spacing: 0.5px;
+    line-height: 50px;
+    padding: 0 35px 0 35px;
+    font-size: 15px;
+    background-color: black;
+    color: white;
+    text-transform: uppercase;
+    font-family: 'Open Sans Condensed';
+    font-weight: bolder;
+    border: none;
+    cursor: pointer;
+
+    &:hover {
+        background-color: white;
+        color: black;
+        border: 1px solid black;
+    }
+`;
